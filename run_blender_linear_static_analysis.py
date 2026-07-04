d3b7e0cdbb50fd70b1acea13c86e4fd5085e6041
Shana Li <shanaxmli@gmail.com>
2026-07-04 14:06:24 -0400

    add correct supports

--- run_blender_linear_static_analysis.py
+++ run_blender_linear_static_analysis.py
@@ -11,14 +11,14 @@ from Pynite import FEModel3D
 import bpy
 import bmesh
 
-# Get the active mesh
+# Get the active mesh; TODO can we select out of multiple elements/only the visible ones in viewport?
 me = bpy.context.object.data
 
 # Get a BMesh representation
 bm = bmesh.new()   # create an empty BMesh
 bm.from_mesh(me)   # fill it in from a Mesh
 
-# Create a FE Model
+# Create an FE Model
 frame = FEModel3D()
 
 # ref: https://pynite.readthedocs.io/en/latest/FEModel3D.html#quick-start
@@ -32,18 +32,21 @@ for i, v in enumerate(bm.verts):
 frame.add_material('A36', E=1, G=2, nu=0.3, rho=0.0004)
 frame.add_section('Wsect', A=5, Iy=6, Iz=7, J=8)
 
+# Add 3D model members for every edge in the mesh
 for i, e in enumerate(bm.edges):
     i_node = f'N{e.verts[0].index}'
     j_node = f'N{e.verts[1].index}'
     frame.add_member(f'M{i}', i_node=i_node, j_node=j_node, material_name='A36', section_name='Wsect')
 
-# TODO: use point/nodal supports
-frame.def_support('N1', support_DX=True, support_DY=True, support_DZ=True, support_RY=True, support_RZ=True)
+# Supports from translation/rotation from every axis
+frame.def_support('N1', support_DX=True, support_DY=True, support_DZ=True, support_RX=True, support_RY=True, support_RZ=True)
+
+# TODO: add point loads
 frame.add_node_load('N2', direction='FZ', P=-5.0, case='D')
 frame.add_load_combo('1.0D', {'D': 1.0})
 
 print('Performing linear analysis')
-frame.analyze_linear(log=True, check_stability=False)  # TODO: try with log=False
+frame.analyze_linear(log=True, check_stability=False)  # TODO: try with log=False; what does this change?
 
 # Results
 uz = frame.nodes['N2'].DZ['1.0D']
